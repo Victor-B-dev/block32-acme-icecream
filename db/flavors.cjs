66777ef367093458f7664c75f779795003d909dc
Victor <ban.victor728@gmail.com>
2024-08-07 13:31:36 -0400

    Added 5 routes requested, creating functions in the flavors for each type of request. Only currently missing returning created & updated flavor from as responses.

--- db/flavors.cjs
+++ db/flavors.cjs
@@ -4,11 +4,68 @@ const createFlavor = async(name, isfav) => {
   try {
     await client.query(`
       INSERT INTO flavors (name, isfav)
-      VALUES ($1, $2);
+      VALUES ($1, $2)
+      RETURNING *;
     `, [name, isfav]);
   } catch(err) {
     console.log(err);
   }
 }
 
-module.exports = {createFlavor}
+const getAllFlavors = async() => {
+  try {
+    const { rows } = await client.query(`
+      SELECT * FROM flavors;
+    `);
+
+    return rows;
+  } catch(err){
+    console.log(err)
+  }
+}
+
+const getFlavor = async (id) => {
+  try{
+    const { rows: [ flavor ] } = await client.query(`
+      SELECT * FROM flavors
+      WHERE id= '${id}';
+      `)
+
+    return flavor
+  } catch (err){
+    console.log(err)
+  }
+}
+
+const deleteFlavor = async(id) => {
+  try{
+    await client.query(`
+      SELECT * FROM flavors
+      WHERE id='${id}';
+      `);
+  } catch (err){
+    console.log(err)
+  }
+}
+
+const updateFlav = async (id, flavorName, isfav) => {
+  try{
+    await client.query(`
+      UPDATE flavors
+      SET name='${flavorName}',
+          isfav='${isfav}'
+      WHERE id='${id}';
+      `);
+  } catch (err){
+    console.log(err)
+  }
+}
+
+
+module.exports = {
+  createFlavor,
+  getAllFlavors,
+  getFlavor,
+  deleteFlavor,
+  updateFlav
+}
