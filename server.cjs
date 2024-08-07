b3ba30068118ba74ad65d70387b71cd73535ee33
Victor <ban.victor728@gmail.com>
2024-08-07 10:20:35 -0400

    initial commit, set up basic connection to DB & server

---
+++ server.cjs
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const client = require('./db/client.cjs');
+client.connect();
+
+const express = require('express')
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
